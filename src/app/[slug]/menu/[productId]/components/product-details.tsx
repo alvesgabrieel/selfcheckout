6f9fceaf998ea3cbf5c39978c44072fd26839816
Gabriel Alves <gabriel_alvesqm@hotmail.com>
2025-02-21 14:15:22 -0300

    feat: add scroll to product details page

--- src/app/[slug]/menu/[productId]/components/product-details.tsx
+++ src/app/[slug]/menu/[productId]/components/product-details.tsx
@@ -7,6 +7,7 @@ import { useState } from "react";
 
 import { formatCurrency } from "@/app/helpers/format-currency";
 import { Button } from "@/components/ui/button";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 interface ProductDetailsProps {
     product: Prisma.ProductGetPayload<{
@@ -37,20 +38,20 @@ const ProductDetails = ({product}: ProductDetailsProps) => {
     }
 
     return ( 
-        <div className="relative z-50 mt-[-1.5rem] rounded-t-3xl p-5 flex-auto flex flex-col">
+        <div className="relative z-50 mt-[-1.5rem] rounded-t-3xl p-5 flex-auto flex flex-col overflow-hidden">
             
-                <div className="flex-auto">
+                <div className="flex-auto overflow-hidden">
                     {/* RESTAURANT */}
                     <div className="flex items-center gap-1.5">
                         <Image src={product.restaurant.avatarImageUrl} alt={product.restaurant.name} width={16} height={16} className="rounded-full"/>
-                        <p className="text-xs text-muted-foreground gap-1">{product.restaurant.name}</p>
+                        <p className="text-xs text-muted-foreground">{product.restaurant.name}</p>
                     </div>
 
                     {/* PRODUCT NAME */}
                     <h2 className="mt-1 text-xl font-semibold">{product.name}</h2>
 
                     {/* PRICE AND QUANTITY */}
-                    <div className="flex items-center justify-between">
+                    <div className="flex items-center justify-between mt-3">
                         <h3 className="text-xl font-semibold">
                             {formatCurrency(product.price)}
                         </h3>
@@ -65,23 +66,29 @@ const ProductDetails = ({product}: ProductDetailsProps) => {
                         </div>
                     </div>
 
-                    {/* PRICE AND QUANTITY */}
-                    <div className="mt-6 space-y-6">
-                        <h4 className="font-semibold">Sobre</h4>
-                        <p className="text-sm text-muted-foreground">{product.description}</p>
-                    </div>
-
-                    {/* PRICE AND QUANTITY */}
-                    <div className="mt-6 space-y-6">
-                        <div className="flex items-center gap-1">
-                            <ChefHatIcon />
-                            <h4 className="font-semibold">Ingredientes</h4>
+                    <ScrollArea className="h-full">
+                        
+                        {/* ABOUT */}
+                        <div className="mt-6 space-y-3">
+                            <h4 className="font-semibold">Sobre</h4>
+                            <p className="text-sm text-muted-foreground">{product.description}</p>
                         </div>
-                        <p className="text-sm text-muted-foreground">{product.description}</p>
-                    </div>
+
+                        {/* INGREDIENTS*/}
+                        <div className="mt-6 space-y-3">
+                            <div className="5 flex items-center gap-1">
+                                <ChefHatIcon />
+                                <h4 className="font-semibold">Ingredientes</h4>
+                            </div>
+                            <ul className="text-muted-fo list-disc px-5 text-sm text-muted-foreground">
+                                {product.ingredients.map(ingredients => <li key={ingredients}>{ingredients}</li>)}
+                            </ul>
+                        </div>
+                    </ScrollArea>
+                    
                 </div>
 
-                <Button className="w-full rounded-full mt-6">Adicionar à sacola</Button>
+                <Button className="w-full rounded-full">Adicionar à sacola</Button>
             
         </div>
      );
