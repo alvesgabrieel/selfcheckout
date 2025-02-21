f2606eddd82a383a9463d166aca43cedb123e936
Gabriel Alves <gabriel_alvesqm@hotmail.com>
2025-02-21 15:17:13 -0300

    feat: add initial cart context and cart sheet component

--- src/app/[slug]/menu/[productId]/components/product-details.tsx
+++ src/app/[slug]/menu/[productId]/components/product-details.tsx
@@ -3,11 +3,14 @@
 import { Prisma } from "@prisma/client";
 import { ChefHatIcon, ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
 import Image from "next/image";
-import { useState } from "react";
+import { useContext, useState } from "react";
 
 import { formatCurrency } from "@/app/helpers/format-currency";
 import { Button } from "@/components/ui/button";
 import { ScrollArea } from "@/components/ui/scroll-area";
+
+import CartSheet from "../../components/cart-sheet";
+import { CartContext } from "../../context/cart";
 
 interface ProductDetailsProps {
     product: Prisma.ProductGetPayload<{
@@ -23,6 +26,7 @@ interface ProductDetailsProps {
 }
 
 const ProductDetails = ({product}: ProductDetailsProps) => {
+    const { toggleCart } = useContext(CartContext)
     const [quantity, setQuantity] = useState<number>(7)
 
     const handleDecreaseQuantity = () => {
@@ -37,9 +41,14 @@ const ProductDetails = ({product}: ProductDetailsProps) => {
         setQuantity((prev) => prev + 1)
     }
 
+    const handleAddToCart = () => {
+        toggleCart()
+    }
+
     return ( 
-        <div className="relative z-50 mt-[-1.5rem] rounded-t-3xl p-5 flex-auto flex flex-col overflow-hidden">
-            
+        <>
+            <div className="relative z-50 mt-[-1.5rem] rounded-t-3xl p-5 flex-auto flex flex-col overflow-hidden">
+                
                 <div className="flex-auto overflow-hidden">
                     {/* RESTAURANT */}
                     <div className="flex items-center gap-1.5">
@@ -88,9 +97,12 @@ const ProductDetails = ({product}: ProductDetailsProps) => {
                     
                 </div>
 
-                <Button className="w-full rounded-full">Adicionar à sacola</Button>
+                <Button className="w-full rounded-full" onClick={handleAddToCart}>Adicionar à sacola</Button>
             
-        </div>
+            </div>
+            <CartSheet />
+            
+        </>
      );
 }
  
