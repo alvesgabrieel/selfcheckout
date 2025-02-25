f19a7557caef597dcacd7d06304195dfb65717dd
Gabriel Alves <gabriel_alvesqm@hotmail.com>
2025-02-25 14:33:40 -0300

    feat: ensure cart summary is shown on restaurant page

--- src/app/[slug]/menu/context/cart.tsx
+++ src/app/[slug]/menu/context/cart.tsx
@@ -3,124 +3,110 @@
 import { Product } from "@prisma/client";
 import { createContext, ReactNode, useState } from "react";
 
-export interface CartProduct extends Pick<Product, "id" | "name" | "price" | "imageUrl"> {
-    quantity: number
+export interface CartProduct
+  extends Pick<Product, "id" | "name" | "price" | "imageUrl"> {
+  quantity: number;
 }
 
 export interface ICartContext {
-    isOpen: boolean,
-    products: CartProduct[],
-    toggleCart: () => void 
-    addProduct: (product: CartProduct) => void
-    decreaseProductQuantity: (productId: string) => void
-    increaseProductQuantity: (productId: string) => void
-    removeProduct: (productId: string) => void
-    total: number,
+  isOpen: boolean;
+  products: CartProduct[];
+  total: number;
+  totalQuantity: number;
+  toggleCart: () => void;
+  addProduct: (product: CartProduct) => void;
+  decreaseProductQuantity: (productId: string) => void;
+  increaseProductQuantity: (productId: string) => void;
+  removeProduct: (productId: string) => void;
 }
 
-//Contexto do carrinho
 export const CartContext = createContext<ICartContext>({
-    isOpen: false,
-    products: [],
-    toggleCart: () => {},
-    addProduct: () => {},
-    decreaseProductQuantity: () => {},
-    increaseProductQuantity: () => {},
-    removeProduct: () => {},
-    total: 0
-})
+  isOpen: false,
+  total: 0,
+  totalQuantity: 0,
+  products: [],
+  toggleCart: () => {},
+  addProduct: () => {},
+  decreaseProductQuantity: () => {},
+  increaseProductQuantity: () => {},
+  removeProduct: () => {},
+});
 
-export const CartProvider = ({children} : {children: ReactNode}) => {
-    const [products, setProducts] = useState<CartProduct[]>([]);
-    const [isOpen, setIsOpen] = useState<boolean>(false);
+export const CartProvider = ({ children }: { children: ReactNode }) => {
+  const [products, setProducts] = useState<CartProduct[]>([]);
+  const [isOpen, setIsOpen] = useState<boolean>(false);
 
-    const toggleCart = () => {
-        setIsOpen(prev => !prev)
+  const total = products.reduce((acc, product) => {
+    return acc + product.price * product.quantity;
+  }, 0);
+  const totalQuantity = products.reduce((acc, product) => {
+    return acc + product.quantity;
+  }, 0);
+  const toggleCart = () => {
+    setIsOpen((prev) => !prev);
+  };
+  const addProduct = (product: CartProduct) => {
+    const productIsAlreadyOnTheCart = products.some(
+      (prevProduct) => prevProduct.id === product.id,
+    );
+    if (!productIsAlreadyOnTheCart) {
+      return setProducts((prev) => [...prev, product]);
     }
-
-    const addProduct = (product: CartProduct) => {
-        
-        const productIsAlreadyOnTheCart = products.some((prevProduct) => prevProduct.id == product.id)
-
-        if(!productIsAlreadyOnTheCart) {
-            return setProducts(prev => [...prev, product])
+    setProducts((prevProducts) => {
+      return prevProducts.map((prevProduct) => {
+        if (prevProduct.id === product.id) {
+          return {
+            ...prevProduct,
+            quantity: prevProduct.quantity + product.quantity,
+          };
         }
-
-        setProducts((prevProducts) => {
-            return prevProducts.map((prevProducts) => {
-                if(prevProducts.id == product.id) {
-                    return {
-                        ...prevProducts,
-                        quantity: prevProducts.quantity + product.quantity
-                    }
-                }
-                return prevProducts;
-            })
-        })
-
-    }
-    
-    // > DIMINUIR A QUANTIDADE DO PRODUTO QUE JÁ ESTÁ NO CARRINHO
-    const decreaseProductQuantity = (productId: string) => {
-        setProducts(prevProducts => {
-            return prevProducts.map(prevProduct => {
-
-                if(prevProduct.id != productId) {
-                    return prevProduct
-                }
-                
-                if(prevProduct.quantity == 1) {
-                    return prevProduct
-                }
-
-                return {...prevProduct, quantity: prevProduct.quantity - 1}
-                
-            })
-        })
-    }
-
-    // > AUMENTAR A QUANTIDADE DO PRODUTO QUE JÁ ESTÁ NO CARRINHO
-    const increaseProductQuantity = (productId: string) => {
-        setProducts(prevProducts => {
-            return prevProducts.map(prevProduct => {
-
-                if(prevProduct.id != productId) {
-                    return prevProduct
-                }
-                
-                if(prevProduct.quantity == 1) {
-                    return prevProduct
-                }
-
-                return {...prevProduct, quantity: prevProduct.quantity + 1}
-                
-            })
-        })
-    }
-
-    // > REMOVER O PRODUTO DO CARRINHO
-    const removeProduct = (productId: string) => {
-        setProducts(prevProducts => prevProducts.filter(prevProduct => prevProduct.id != productId))
-    }
-
-    // > CALCULAR PREÇO TOTAL DO CARRINHO
-    const total = products.reduce((acc, product) => {
-        return acc + product.price * product.quantity 
-    }, 0)
-
-    return (
-        <CartContext.Provider value={{
-            isOpen,
-            products,
-            toggleCart,
-            addProduct,
-            decreaseProductQuantity,
-            increaseProductQuantity,
-            removeProduct,
-            total
-        }}>
-
-            {children}
-        </CartContext.Provider>
-    )
-}
+        return prevProduct;
+      });
+    });
+  };
+  const decreaseProductQuantity = (productId: string) => {
+    setProducts((prevProducts) => {
+      return prevProducts.map((prevProduct) => {
+        if (prevProduct.id !== productId) {
+          return prevProduct;
+        }
+        if (prevProduct.quantity === 1) {
+          return prevProduct;
+        }
+        return { ...prevProduct, quantity: prevProduct.quantity - 1 };
+      });
+    });
+  };
+  const increaseProductQuantity = (productId: string) => {
+    setProducts((prevProducts) => {
+      return prevProducts.map((prevProduct) => {
+        if (prevProduct.id !== productId) {
+          return prevProduct;
+        }
+        return { ...prevProduct, quantity: prevProduct.quantity + 1 };
+      });
+    });
+  };
+  const removeProduct = (productId: string) => {
+    setProducts((prevProducts) =>
+      prevProducts.filter((prevProduct) => prevProduct.id !== productId),
+    );
+  };
+  return (
+    <CartContext.Provider
+      value={{
+        isOpen,
+        products,
+        toggleCart,
+        addProduct,
+        decreaseProductQuantity,
+        increaseProductQuantity,
+        removeProduct,
+        total,
+        totalQuantity,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
